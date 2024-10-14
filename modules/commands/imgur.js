7641cc7857032819d7a2fb3ec43dc32a2b6248d5
Zainiprince7 <fazooole@gmail.com>
2024-10-15 03:34:57 +0500

    Update imgur.js

--- modules/commands/imgur.js
+++ modules/commands/imgur.js
@@ -1,35 +1,22 @@
 module.exports.config = {
     name: "imgur",
     version: "1.0.0",
-    hasPermission: 0,  // Typo corrected from 'hasPermssion' to 'hasPermission'
-    credits: "Aadi Gupta",
-    description: "Upload an image to Imgur",
-    commandCategory: "Game",
-    usages: "[reply]",
-    cooldowns: 5,
+    hasPermssion: 2,
+    credits: "ZiaRein",
+    description: "imgur upload",
+    commandCategory: "link",
+    usages: `Please reply to image\n\nHow to use?\n${global.config.PREFIX}imgur [reply] <img>\n\nExample:\n${global.config.PRFIX}imgur <img reply>\n`,
+    cooldowns: 1,
     dependencies: {
-        "axios": ""
-    }
+  "axios": "",}
 };
-
-module.exports.run = async ({ api, event, args }) => {
-    const axios = global.nodemodule['axios'];
-    
-    // Check if there is a reply or if an image link is provided
-    var linkanh = event.messageReply ? event.messageReply.attachments[0].url : args.join(" ");
-    if (!linkanh) {
-        return api.sendMessage('Please reply to an image or enter a link to an image!', event.threadID, event.messageID);
-    }
-
-    try {
-        const res = await axios.get(`https://imgur-api-by-koja.xx0xkoja.repl.co/imgur?link=${encodeURIComponent(linkanh)}`);
-        if (res.data && res.data.uploaded && res.data.uploaded.image) {
-            var img = res.data.uploaded.image;
-            return api.sendMessage(`${img}`, event.threadID, event.messageID);
-        } else {
-            return api.sendMessage('Image upload failed. Please try again.', event.threadID, event.messageID);
-        }
-    } catch (error) {
-        return api.sendMessage('An error occurred while uploading the image. Please try again later.', event.threadID, event.messageID);
-    }
-};
+ 
+module.exports.run = async ({ api, event }) => {
+const axios = global.nodemodule['axios'];  
+var ZiaRein = event.messageReply.attachments[0].url || args.join(" ");
+    if(!ZiaRein) return api.sendMessage(`Please reply to image\n\nHow to use?\n${global.config.PREFIX}imgur [reply] <img>\n\nExample:\n${global.config.PRFIX}imgur <img reply>\n\nCreated by: ZiaRein`, event.threadID, event.messageID)
+const res = await axios.get(`https://api.phamvandien.xyz/imgur?link=${encodeURIComponent(ZiaRein)}`);    
+var ZiaReinn = res.data.uploaded.image;
+    return api.sendMessage(ZiaReinn, event.threadID, event.messageID);
+ 
+}
